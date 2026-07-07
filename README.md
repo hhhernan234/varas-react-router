1216b1d08db73777e19aa80072695f5ca7a94707
hhhernan234 <varashernan9@gmail.com>
2026-07-07 07:49:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# varas-react-router
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+// src/pages/HomePage.tsx
+
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 12 }}>Página no encontrada</h1>
+      <p style={{ color: '#6b7280' }}>
+        La página que buscas no existe.
+      </p>
+    </div>
+  )
+}
+  
